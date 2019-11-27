4a7f449634df2c00b5055b29b109e3fc7fc67189
d7p4n4 <bercikezs199999999@gmail.com>
2019-11-27 16:25:21 +0100

    Add project files.

---
+++ CSClient1/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CSClient1.Models;
+
+namespace CSClient1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Client()
+        {
+            return View();
+        }
+
+    }
+}
